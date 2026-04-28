15ebea8c5aa3231f2a515219a4b84408902e8933
Wlissis <wlissistech@gmail.com>
2026-04-28 11:04:02 -0300

    adicionando rota de buscar todos os pokemons e buscar um unico pokemons, validando e tratando erros

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from .models import PokemonBase, PokemonLendario
 from typing import List
 
@@ -13,7 +13,36 @@ pokedex_db: List[Union[PokemonBase, PokemonLendario]] = []
 # O FastAPI usa o campo 'is_lendario' para decidir qual classe usar
 PokemonUnion = Annotated[Union[PokemonLendario, PokemonBase], Field(discriminator='is_lendario')]
 
-@app.post("/pokemons",status_code=201)
+@app.post("/pokemons",status_code=status.HTTP_201_CREATED)
 def cadastrar_pokemon(pokemon:PokemonUnion):
+    """
+    adiciona um pokemon 
+    """
+    for p in pokedex_db:
+        if p.id == pokemon.id:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, 
+                detail="Este ID já existe na Pokedex!"
+            )
     pokedex_db.append(pokemon)
-    return {"status": "Registrado", "tipo": pokemon.is_lendario}
+    return {"message": "Pokémon cadastrado com sucesso", "pokemon": pokemon}
+
+@app.get("/pokemons", status_code=status.HTTP_200_OK)
+def listar_pokemons():
+    """
+    Retorna uma lista contendo todos os Pokémons cadastrados na Pokedex.
+    """
+    return pokedex_db
+
+
+@app.get("/pokemons/{pokemon_id}", status_code=status.HTTP_200_OK)
+def buscar_pokemon(pokemon_id: int):
+    for pokemon in pokedex_db:
+        if pokemon.id == pokemon_id:
+            return pokemon
+    
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail=f"Pokémon com ID {pokemon_id} não encontrado"
+    )
+   
